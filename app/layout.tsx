f3cc0300af66fc76419ad6b3409dd49c6611b323
asethii <ashinder@gmail.com>
2026-02-10 18:59:43 -0500

    add no index

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,10 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.svg',
   },
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
